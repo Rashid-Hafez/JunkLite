49f0649fd1a85ae80cac6199e91727c902c6ffc7
Ali <aliahmadalani@gmail.com>
2026-02-15 14:02:03 -0600

    Freeze Y rotation on character (otherwise the camera switch colliders cause character to rotate when hitting it)

--- Assets/Game/Scripts/Character/Character2D5Controller.cs
+++ Assets/Game/Scripts/Character/Character2D5Controller.cs
@@ -203,7 +203,7 @@ namespace junklite
             Vector3 mostPerpendicularAxis =
                 (dotX < dotZ) ? Vector3.right : Vector3.forward;
 
-            RigidbodyConstraints constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+            RigidbodyConstraints constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
 
             if (mostPerpendicularAxis == Vector3.right)
                 constraints |= RigidbodyConstraints.FreezePositionX;
